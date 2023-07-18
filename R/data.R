74ec252719f80967eeeee6ac5c929f0184fcfb1e
kaz462 <kangjie.zhang@bayer.com>
2023-07-18 14:33:06 -0700

    #26: update data.R

--- R/data.R
+++ R/data.R
@@ -3,28 +3,21 @@
 #' An updated SDTM AE dataset that uses the CDISC pilot project
 #'
 #' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_ae.rda}
-"admiral_ae"
-
-#' Adverse Events Dataset-raw
-#'
-#' A SDTM AE dataset from the CDISC pilot project
-#'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt?raw=true} # nolint
-"raw_ae"
+"ae"
 
 #' Concomitant Medication Dataset
 #'
 #' A SDTM CM dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/cm.xpt?raw=true} # nolint
-"admiral_cm"
+"cm"
 
 #' Demography Dataset
 #'
 #' A SDTM DM dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt?raw=true} # nolint
-"admiral_dm"
+"dm"
 
 #' Disposition Dataset-updated
 #'
@@ -32,27 +25,20 @@
 #'
 #' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_ds.rda}
 #' @author Gopi Vegesna
-"admiral_ds"
-
-#' Disposition Dataset-raw
-#'
-#' A SDTM DS dataset from the CDISC pilot project
-#'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ds.xpt?raw=true} # nolint
-"raw_ds"
+"ds"
 
 #' Electrocardiogram Dataset
 #'
 #' An example of standard SDTM EG dataset to be used in deriving ADEG dataset
 #'
-"admiral_eg"
+"eg"
 
 #' Exposure Dataset
 #'
 #' A SDTM EX dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt?raw=true} # nolint
-"admiral_ex"
+"ex"
 
 #' Ophthalmology Exposure Dataset
 #'
@@ -67,14 +53,7 @@
 #'
 #' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_lb.rda}
 #' @author Annie Yang
-"admiral_lb"
-
-#' Laboratory Measurements Dataset
-#'
-#' A SDTM LB dataset from the CDISC pilot project
-#'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt?raw=true} # nolint
-"raw_lb"
+"lb"
 
 #' Medical History Dataset-updated
 #'
@@ -82,21 +61,14 @@
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt?raw=true} # nolint
 #' @author Annie Yang
-"admiral_mh"
-
-#' Medical History Dataset-raw
-#'
-#' A SDTM MH dataset from the CDISC pilot project
-#'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt?raw=true} # nolint
-"raw_mh"
+"mh"
 
 #' Ophthalmic Examinations Dataset
 #'
 #' A SDTM OE dataset simulated by Ophthalmology team
 #'
 #' @author Gordon Miller
-"admiral_oe"
+"oe"
 
 #' Pharmacokinetics Concentrations Dataset
 #'
@@ -104,7 +76,7 @@
 #'
 #' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_pc.rda}
 #' @author Antonio Rodríguez Contestí
-"admiral_pc"
+"pc"
 
 #' Pharmacokinetic Parameters Dataset
 #'
@@ -112,21 +84,14 @@
 #'
 #' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_pp.rda}
 #' @author Antonio Rodríguez Contestí
-"admiral_pp"
-
-#' Questionnaire Dataset
-#'
-#' A SDTM QS dataset from the CDISC pilot project
-#'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/qs.xpt?raw=true} # nolint
-"raw_qs"
+"pp"
 
 #' Questionnaire Dataset
 #'
 #' A SDTM QS dataset from the CDISC pilot project & Ophthalmology test data
 #'
 #' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_qs.rda} # nolint
-"admiral_qs"
+"qs"
 
 #' Ophthalmology Questionnaire Dataset
 #'
@@ -140,93 +105,86 @@
 #' A SDTM SC dataset simulated by Ophthalmology team
 #'
 #' @author Gordon Miller
-"admiral_sc"
+"sc"
 
 #' Tumor Identification Dataset
 #'
 #' A SDTM TU dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
-"admiral_tu"
+"tu"
 
 #' Tumor Results Dataset
 #'
 #' A SDTM TR dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
-"admiral_tr"
+"tr"
 
 #' Disease Response Dataset
 #'
 #' A SDTM RS dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
-"admiral_rs"
+"rs"
 
 #' Supplemental Adverse Events Dataset
 #'
 #' A SDTM SUPPAE dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppae.xpt?raw=true} # nolint
-"admiral_suppae"
+"suppae"
 
 #' Supplemental Disposition Dataset-updated
 #'
 #' A SDTM SUPPDS dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt?raw=true} # nolint
-"admiral_suppds"
-
-#' Supplemental Disposition Dataset-raw
-#'
-#' A SDTM SUPPDS dataset from the CDISC pilot project
-#'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt?raw=true} # nolint
-"raw_suppds"
+"suppds"
 
 #' Supplemental Demography Dataset
 #'
 #' A SDTM SUPPDM dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppdm.xpt?raw=true} # nolint
-"admiral_suppdm"
+"suppdm"
 
 #' Supplemental Tumor Results Dataset
 #'
 #' A SDTM SUPPTR dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
-"admiral_supptr"
+"supptr"
 
 #' Trial Design Dataset
 #'
 #' A SDTM TS dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ts.xpt?raw=true} # nolint
-"admiral_ts"
+"ts"
 
 #' Vital Signs Dataset
 #'
 #' A SDTM VS dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/vs.xpt?raw=true} # nolint
-"admiral_vs"
+"vs"
 
 #' SMQ Dataset
 #'
 #' An example SMQ dataset
 #'
-"admiral_smq_db"
+"smq_db"
 
 #' SDG Dataset
 #'
 #' An example SDG dataset
 #'
-"admiral_sdg_db"
+"sdg_db"
 
 #' Subject Visits Dataset
 #'
 #' A SDTM SV dataset from the CDISC pilot project
 #'
 #' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/sv.xpt?raw=true} # nolint
-"admiral_sv"
+"sv"
