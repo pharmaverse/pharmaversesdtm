6b66b152ec5577f47d246691227707d2539ef039
Pooja Kumari <pooja.2.kumari@gsk.com>
2024-02-12 13:17:05 +0530

    #82 Resolve Bug: Duplicate observation for group USUBJID and VISIT

--- data-raw/sv.R
+++ data-raw/sv.R
@@ -4,8 +4,20 @@
 # Load libraries -----
 library(haven)
 library(admiral)
+library(dplyr)
 raw_sv <- read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/sv.xpt?raw=true") # nolint
 sv <- convert_blanks_to_na(raw_sv)
+
+sv <- sv %>%
+  group_by(USUBJID, VISITNUM) %>%
+  mutate(
+    ASEQ = row_number(),
+    n=n()) %>%
+  mutate(
+    VISIT = ifelse(n > 1, paste0("UNSCHEDULED ", floor(VISITNUM), ".", ASEQ), VISIT),
+    VISITNUM = ifelse(n > 1, as.numeric(paste0(floor(VISITNUM), ".", ASEQ)), VISITNUM)) %>%
+  ungroup() %>%
+  select(-ASEQ,-n)
 
 # Label dataset ----
 attr(sv, "label") <- "Subject Visits"
