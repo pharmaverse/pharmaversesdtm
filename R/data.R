2e44b320368cd86c8a2aca4cef215108fed66a29
sadchla-codes <sadchla.mathieu@gmail.com>
2024-05-28 00:36:01 -0400

    Closes #83 updating the dataset namings.

--- R/data.R
+++ R/data.R
@@ -1,32 +1,32 @@
-#' Adverse Events Dataset-updated
+#' Adverse Events
 #'
 #' An updated SDTM AE dataset that uses the CDISC pilot project
 #'
 #' @source \url{https://github.com/pharmaverse/pharmaversesdtm/blob/main/data/ae.rda}
 "ae"
 
-#' Ophthalmology Adverse Events Dataset
+#' Adverse Events for Ophthalmology
 #'
 #' An example Adverse Events SDTM dataset with ophthalmology-specific variable `AELAT`
 #'
-#' @source Constructed using `ae` from the `{pharmaversesdtm}` package # nolint
+#' @source Constructed using `ae` from the `{pharmaversesdtm}` package  # nolint
 "ae_ophtha"
 
-#' Concomitant Medication Dataset
+#' Concomitant Medication
 #'
 #' A SDTM CM dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/cm.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/cm.xpt?raw=true}  # nolint
 "cm"
 
-#' Demography Dataset
+#' Demography
 #'
 #' A SDTM DM dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt?raw=true}  # nolint
 "dm"
 
-#' Disposition Dataset-updated
+#' Disposition
 #'
 #' An updated SDTM DS dataset that uses the CDISC pilot project
 #'
@@ -34,27 +34,27 @@
 #' @author Gopi Vegesna
 "ds"
 
-#' Electrocardiogram Dataset
+#' Electrocardiogram
 #'
 #' An example of standard SDTM EG dataset to be used in deriving ADEG dataset
 #'
 "eg"
 
-#' Exposure Dataset
+#' Exposure
 #'
 #' A SDTM EX dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt?raw=true}  # nolint
 "ex"
 
-#' Ophthalmology Exposure Dataset
+#' Exposure for Ophthalmology
 #'
 #' An example Exposure SDTM dataset with ophthalmology-specific variables such as `EXLOC` and `EXLAT`
 #'
-#' @source Constructed using `ex` from the `{pharmaversesdtm}` package # nolint
+#' @source Constructed using `ex` from the `{pharmaversesdtm}` package #nolint
 "ex_ophtha"
 
-#' Laboratory Measurements Dataset
+#' Laboratory Measurements
 #'
 #' An updated SDTM LB dataset that uses data from the CDISC pilot project
 #'
@@ -62,22 +62,22 @@
 #' @author Annie Yang
 "lb"
 
-#' Medical History Dataset-updated
+#' Medical History
 #'
 #' An updated SDTM MH dataset that uses data from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt?raw=true}  # nolint
 #' @author Annie Yang
 "mh"
 
-#' Ophthalmic Examinations Dataset
+#' Ophthalmic Examinations for Ophthalmology
 #'
 #' A SDTM OE dataset simulated by Ophthalmology team
 #'
 #' @author Gordon Miller
 "oe_ophtha"
 
-#' Pharmacokinetics Concentrations Dataset
+#' Pharmacokinetic Concentrations
 #'
 #' A SDTM PC dataset simulated by Antonio Rodríguez Contestí
 #'
@@ -85,7 +85,7 @@
 #' @author Antonio Rodríguez Contestí
 "pc"
 
-#' Pharmacokinetic Parameters Dataset
+#' Pharmacokinetic Parameters
 #'
 #' A SDTM PP dataset simulated by Antonio Rodríguez Contestí
 #'
@@ -93,42 +93,42 @@
 #' @author Antonio Rodríguez Contestí
 "pp"
 
-#' Ophthalmology Questionnaire Dataset
+#' Questionnaire for Ophthalmology
 #'
 #' An example Questionnaires SDTM dataset with ophthalmology-specific questionnaire of NEI VFQ-25
 #'
-#' @source Constructed using `qs` from the `{pharmaversesdtm}` package # nolint
+#' @source Constructed using `qs` from the `{pharmaversesdtm}` package #nolint
 "qs_ophtha"
 
-#' Subject Characteristic Dataset
+#' Subject Characteristic for Ophthalmology
 #'
 #' A SDTM SC dataset simulated by Ophthalmology team
 #'
 #' @author Gordon Miller
 "sc_ophtha"
 
-#' Tumor Identification Dataset
+#' Tumor Identification for Oncology
 #'
 #' A SDTM TU dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
 "tu_onco"
 
-#' Tumor Results Dataset
+#' Tumor Results for Oncology
 #'
 #' A SDTM TR dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
 "tr_onco"
 
-#' Disease Response Dataset
+#' Disease Response for Oncology
 #'
 #' A SDTM RS dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
 "rs_onco"
 
-#' Tumor Results Dataset (RECIST 1.1)
+#' Tumor Results (RECIST 1.1) for Oncology
 #'
 #' A SDTM TR dataset using RECIST 1.1. The dataset contains just a few patients.
 #' It is intended for vignettes and examples of ADaM dataset creation.
@@ -136,7 +136,7 @@
 #' @author Stefan Bundfuss
 "tr_onco_recist"
 
-#' Tumor Identification Dataset (RECIST 1.1)
+#' Tumor Identification (RECIST 1.1) for Oncology
 #'
 #' A SDTM TU dataset using RECIST 1.1. The dataset contains just a few patients.
 #' It is intended for vignettes and examples of ADaM dataset creation.
@@ -144,7 +144,7 @@
 #' @author Stefan Bundfuss
 "tu_onco_recist"
 
-#' Disease Response Dataset (RECIST 1.1)
+#' Disease Response (RECIST 1.1) for Oncology
 #'
 #' A SDTM RS dataset using RECIST 1.1. The dataset contains just a few patients.
 #' It is intended for vignettes and examples of ADaM dataset creation.
@@ -154,7 +154,7 @@
 #' @author Stefan Bundfuss
 "rs_onco_recist"
 
-#' Disease Response Dataset (iRECIST)
+#' Disease Response (iRECIST) for Oncology
 #'
 #' A SDTM RS dataset using iRECIST. The dataset contains just a few patients.
 #' It is intended for vignettes and examples of ADaM dataset creation.
@@ -162,141 +162,141 @@
 #' @author Rohan Thampi
 "rs_onco_irecist"
 
-#' Supplemental Adverse Events Dataset
+#' Supplemental Adverse Events
 #'
 #' A SDTM SUPPAE dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppae.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppae.xpt?raw=true}  # nolint
 "suppae"
 
-#' Supplemental Disposition Dataset-updated
+#' Supplemental Disposition
 #'
 #' A SDTM SUPPDS dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt?raw=true}  # nolint
 "suppds"
 
-#' Supplemental Demography Dataset
+#' Supplemental Demography
 #'
 #' A SDTM SUPPDM dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppdm.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppdm.xpt?raw=true}  # nolint
 "suppdm"
 
-#' Supplemental Tumor Results Dataset
+#' Supplemental Tumor Results for Oncology
 #'
 #' A SDTM SUPPTR dataset simulated by Gopi Vegesna
 #'
 #' @author Gopi Vegesna
 "supptr_onco"
 
-#' Trial Design Dataset
+#' Trial Design
 #'
 #' A SDTM TS dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ts.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ts.xpt?raw=true}  # nolint
 "ts"
 
-#' Vital Signs Dataset
+#' Vital Signs
 #'
 #' A SDTM VS dataset from the CDISC pilot project
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/vs.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/vs.xpt?raw=true}  # nolint
 "vs"
 
-#' SMQ Dataset
+#' Standardized MedDRA Queries
 #'
 #' An example SMQ dataset
 #'
 "smq_db"
 
-#' SDG Dataset
+#' SDG
 #'
 #' An example SDG dataset
 #'
 "sdg_db"
 
-#' Subject Visits Dataset
+#' Subject Visits
 #'
 #' A SDTM SV dataset from the CDISC pilot project. Duplicate observation for group
 #' variable `USUBJID` and `VISIT` is corrected.
 #'
-#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/sv.xpt?raw=true} # nolint
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/sv.xpt?raw=true}  # nolint
 "sv"
 
-#' Demographics Dataset for Vaccine Studies
+#' Demographics for Vaccine
 #'
 #' An example SDTM DM dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_dm.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_dm.rda}  # nolint
 "dm_vaccine"
 
-#' Vital Signs Dataset for Vaccine Studies
+#' Vital Signs for Vaccine
 #'
 #' An example SDTM VS dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_vs.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_vs.rda}  # nolint
 "vs_vaccine"
 
-#' Clinical Events Dataset for Vaccine Studies
+#' Clinical Events for Vaccine
 #'
 #' An example SDTM CE dataset for vaccine studies
 #'
 #' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_ce.rda} # nolint
 "ce_vaccine"
 
-#' Exposures Dataset for Vaccine Studies
+#' Exposures for Vaccine
 #'
 #' An example SDTM EX dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_ex.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_ex.rda}  # nolint
 "ex_vaccine"
 
-#' Immunogenicity Specimen Assessments Dataset for Vaccine Studies
+#' Immunogenicity Specimen Assessments for Vaccine
 #'
-#' An example SDTM IS dataset for vaccine studies
+#' An example SDTM IS for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_is.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_is.rda}  # nolint
 "is_vaccine"
 
-#' Findings About Clinical Events Dataset for Vaccine Studies
+#' Findings About Clinical Events for Vaccine
 #'
-#' An example SDTM FACE dataset for vaccine studies
+#' An example SDTM FACE for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_face.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_face.rda}  # nolint
 "face_vaccine"
 
-#' Supplemental Qualifiers for Clinical Events Dataset for Vaccine Studies
+#' Supplemental Qualifiers for Clinical Events for Vaccine
 #'
-#' An example SDTM SUPPCE dataset for vaccine studies
+#' An example SDTM SUPPCE for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppce.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppce.rda}  # nolint
 "suppce_vaccine"
 
-#' Supplemental Qualifiers for Demographics Dataset for Vaccine Studies
+#' Supplemental Qualifiers for Demographics for Vaccine
 #'
 #' An example SDTM SUPPDM dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppdm.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppdm.rda}  # nolint
 "suppdm_vaccine"
 
-#' Supplemental Qualifiers for Exposures Dataset for Vaccine Studies
+#' Supplemental Qualifiers for Exposures for Vaccine
 #'
 #' An example SDTM SUPPEX dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppex.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppex.rda}  # nolint
 "suppex_vaccine"
 
-#' Supplemental Qualifiers for Findings About for Clinical Events Dataset for Vaccine Studies # nolint
+#' Supplemental Qualifiers for Findings About for Clinical Events for Vaccine
 #'
 #' An example SDTM SUPPFACE dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppface.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppface.rda}  # nolint
 "suppface_vaccine"
 
-#' Supplemental Qualifiers for Immunogenicity Specimen Assessments Dataset for Vaccine Studies # nolint
+#' Supplemental Qualifiers for Immunogenicity Specimen Assessments for Vaccine
 #'
 #' An example SDTM SUPPIS dataset for vaccine studies
 #'
-#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppis.rda} # nolint
+#' @source \url{https://github.com/pharmaverse/admiralvaccine/blob/main/data/vx_suppis.rda}  # nolint
 "suppis_vaccine"
