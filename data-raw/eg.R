a79f5e019ab3c3f848d75af8d2cda85373185eba
Vladyslav <shuliarv@roche.com>
2024-07-31 10:40:53 +0000

    #56 code improved, labels and comments added

--- data-raw/eg.R
+++ data-raw/eg.R
@@ -1,8 +1,12 @@
+# Load necessary libraries
 library(dplyr)
+library(labelled)
 
+# Define constants and mappings
 studyid <- "CDISCPILOT01"
 domain <- "EG"
-usubjids <- unique(vs$USUBJID)
+usubjids <-
+  unique(vs$USUBJID) # Get unique subject IDs from vs data frame
 egtestcds <- c("QT", "HR", "RR", "ECGINT")
 visits <- c(
   "SCREENING 1",
@@ -21,14 +25,11 @@ visits <- c(
   "WEEK 26",
   "RETRIEVAL"
 )
-
 egtpts <- c(
   "AFTER LYING DOWN FOR 5 MINUTES",
   "AFTER STANDING FOR 1 MINUTE",
   "AFTER STANDING FOR 3 MINUTES"
 )
-
-
 visitnum_mapping <- c(
   "SCREENING 1" = 1,
   "SCREENING 2" = 2,
@@ -46,7 +47,6 @@ visitnum_mapping <- c(
   "WEEK 26" = 13,
   "RETRIEVAL" = 201
 )
-
 visitdy_mapping <- c(
   "SCREENING 1" = -7,
   "SCREENING 2" = -1,
@@ -64,33 +64,28 @@ visitdy_mapping <- c(
   "WEEK 26" = 182,
   "RETRIEVAL" = 168
 )
-
 egtptnum_mapping <- c(
   "AFTER LYING DOWN FOR 5 MINUTES" = 815,
   "AFTER STANDING FOR 1 MINUTE" = 816,
   "AFTER STANDING FOR 3 MINUTES" = 817
 )
-
 egeltm_mapping <- c(
   "AFTER LYING DOWN FOR 5 MINUTES" = "PT5M",
   "AFTER STANDING FOR 1 MINUTE" = "PT1M",
   "AFTER STANDING FOR 3 MINUTES" = "PT3M"
 )
-
 egtest_mapping <- c(
   "QT" = "QT Duration",
   "HR" = "Heart Rate",
   "RR" = "RR Duration",
   "ECGINT" = "ECG Interpretation"
 )
-
 egorresu_mapping <- c(
   "QT" = "mmHg",
   "HR" = "BEATS/MIN",
   "RR" = "mmHg",
   "ECGINT" = "LB"
 )
-
 egstresu_mapping <- c(
   "QT" = "msec",
   "HR" = "BEATS/MIN",
@@ -98,71 +93,95 @@ egstresu_mapping <- c(
   "ECGINT" = NA
 )
 
-egdtc = select(vs, USUBJID, VISIT, VSDTC) %>% distinct()
+# Extract unique date/time of measurements for each subject and visit
+egdtc <- vs %>%
+  select(USUBJID, VISIT, VSDTC) %>%
+  distinct() %>%
+  rename(EGDTC = VSDTC)
 
+# Set seed for reproducibility
+set.seed(123)
 
+# Function to generate patient data
 add_patient <- function(usubjid) {
-  patient <-  expand.grid(
+  patient <- expand.grid(
     USUBJID = usubjid,
     EGTESTCD = egtestcds,
     EGTPT = egtpts,
     VISIT = visits
-  )  %>% arrange(EGTESTCD) %>%
+  ) %>%
+    arrange(EGTESTCD) %>%
     group_by(VISIT) %>%
-    filter(!(EGTESTCD == "ECGINT" & duplicated(EGTESTCD))) %>%
-    ungroup() %>% left_join(egdtc, by = c("USUBJID", "VISIT")) %>%
+    filter(!(
+      EGTESTCD == "ECGINT" &
+        VISIT %in% c(
+          "AMBUL ECG PLACEMENT",
+          "AMBUL ECG REMOVAL",
+          "RETRIEVAL",
+          "SCREENING 2"
+        )
+    ) &
+      !(EGTESTCD == "ECGINT" & duplicated(EGTESTCD))) %>%
+    ungroup() %>%
+    left_join(egdtc, by = c("USUBJID", "VISIT")) %>%
+    filter(!is.na(EGDTC)) %>%
     mutate(
       STUDYID = studyid,
-      USUBJID = as.character(USUBJID),
       DOMAIN = domain,
-      EGSEQ = row_number(),
+      EGSEQ = as.numeric(row_number()),
+      # Generate sequence number
       EGTESTCD = as.character(EGTESTCD),
       EGTEST = egtest_mapping[EGTESTCD],
       EGORRESU = egorresu_mapping[EGTESTCD],
-      EGBLFL = ifelse(VISIT == "BASELINE", "Y", ""),
-      VISITNUM = visitnum_mapping[VISIT],
-      VISITDY = visitdy_mapping[VISIT],
-      EGTPTNUM = ifelse(EGTESTCD == "ECGINT", NA, egtptnum_mapping[EGTPT]),
       EGELTM = ifelse(EGTESTCD == "ECGINT", "", egeltm_mapping[EGTPT]),
-      EGTPTREF = ifelse(
-        EGTESTCD == "ECGINT",
-        "",
-        ifelse(EGELTM == "PT5M", "PATIENT SUPINE", "PATIENT STANDING")
-      ),
-      EGSTAT = "",
-      EGSTRESU = egstresu_mapping[EGTESTCD],
+      # Generate random results based on test type and time point
       EGSTRESN = case_when(
-        EGTESTCD == "RR" &
-          EGELTM == "PT5M" ~ floor(rnorm(n(), 543.9985, 80)),
-        EGTESTCD == "RR" &
-          EGELTM == "PT3M" ~ floor(rnorm(n(), 536.0161, 80)),
-        EGTESTCD == "RR" &
-          EGELTM == "PT1M" ~ floor(rnorm(n(), 532.3233, 80)),
-        EGTESTCD == "HR" &
-          EGELTM == "PT5M" ~ floor(rnorm(n(), 70.04389, 10)),
-        EGTESTCD == "HR" &
-          EGELTM == "PT3M" ~ floor(rnorm(n(), 74.27798, 10)),
-        EGTESTCD == "HR" &
-          EGELTM == "PT1M" ~ floor(rnorm(n(), 74.77461, 10)),
-        EGTESTCD == "QT" &
-          EGELTM == "PT5M" ~ floor(rnorm(n(), 450.9781, 60)),
-        EGTESTCD == "QT" &
-          EGELTM == "PT3M" ~ floor(rnorm(n(), 457.7265, 60)),
-        EGTESTCD == "QT" &
-          EGELTM == "PT1M" ~ floor(rnorm(n(), 455.3394, 60))
+        EGTESTCD == "RR" ~ floor(rnorm(
+          n(), ifelse(
+            EGELTM == "PT5M",
+            543.9985,
+            ifelse(EGELTM == "PT3M", 536.0161, 532.3233)
+          ), 80
+        )),
+        EGTESTCD == "HR" ~ floor(rnorm(
+          n(), ifelse(
+            EGELTM == "PT5M",
+            70.04389,
+            ifelse(EGELTM == "PT3M", 74.27798, 74.77461)
+          ), 8
+        )),
+        EGTESTCD == "QT" ~ floor(rnorm(
+          n(), ifelse(
+            EGELTM == "PT5M",
+            450.9781,
+            ifelse(EGELTM == "PT3M", 457.7265, 455.3394)
+          ), 60
+        ))
       ),
       EGSTRESC = ifelse(EGTESTCD == "ECGINT", sample(c(
         "NORMAL", "ABNORMAL"
       ), 1), as.character(EGSTRESN)),
       EGORRES = EGSTRESC,
+      EGSTRESU = egstresu_mapping[EGTESTCD],
+      EGSTAT = "",
       EGLOC = "",
+      EGBLFL = ifelse(VISIT == "BASELINE", "Y", ""),
+      VISITNUM = visitnum_mapping[VISIT],
+      VISITDY = visitdy_mapping[VISIT],
+      EGTPTNUM = ifelse(EGTESTCD == "ECGINT", NA, egtptnum_mapping[EGTPT]),
+      EGTPTREF = ifelse(
+        EGTESTCD == "ECGINT",
+        "",
+        ifelse(EGELTM == "PT5M", "PATIENT SUPINE", "PATIENT STANDING")
+      ),
       EGDY = VISITDY,
-      EGTPT = ifelse(EGTESTCD == "ECGINT", "", EGTPT),
-      EGDTC = VSDTC
-    ) %>%  filter(!is.na(EGDTC)) %>% filter(!(EGTESTCD == "ECGINT" & (VISIT == "AMBUL ECG PLACEMENT" | VISIT == "AMBUL ECG REMOVAL" | VISIT == "RETRIEVAL" | VISIT == "SCREENING 2")))
+      EGTPT = ifelse(EGTESTCD == "ECGINT", "", EGTPT)
+    )
+  return(patient)
 }
 
-patients <- lapply(usubjids, add_patient) %>%
+# Apply the add_patient function to each subject ID and combine results
+eg_test <- lapply(usubjids, add_patient) %>%
   bind_rows()  %>%
   select(
     STUDYID,
@@ -190,7 +209,36 @@ patients <- lapply(usubjids, add_patient) %>%
     EGTPTREF
   )
 
-View(patients)
-# HR - 40 to 134
-# RR - 280 to 868
-# QT - 234 to 708
+# Add labels to the columns
+var_label(eg_test) <- list(
+  STUDYID = "Study Identifier",
+  DOMAIN = "Domain Abbreviation",
+  USUBJID = "Unique Subject Identifier",
+  EGSEQ = "Sequence Number",
+  EGTESTCD = "Test Code",
+  EGTEST = "Test Name",
+  EGORRES = "Original Result",
+  EGORRESU = "Original Units",
+  EGELTM = "Elapsed Time",
+  EGSTRESC = "Standard Character Result",
+  EGSTRESN = "Standard Numeric Result",
+  EGSTRESU = "Standard Units",
+  EGSTAT = "Completion Status",
+  EGLOC = "Location of Vital Signs Measurement",
+  EGBLFL = "Baseline Flag",
+  VISITNUM = "Visit Number",
+  VISIT = "Visit Name",
+  VISITDY = "Planned Study Day of Visit",
+  EGDTC = "Date/Time of Measurements",
+  EGDY = "Study Day of Vital Signs",
+  EGTPT = "Planned Time Point Number",
+  EGTPTNUM = "Time Point Number",
+  EGELTM = "Planned Elapsed Time from Time Point Ref",
+  EGTPTREF = "Time Point Reference"
+)
+
+# Label the dataset
+attr(eg_test, "label") <- "Electrocardiogram"
+
+# Save the dataset
+usethis::use_data(eg_test, overwrite = TRUE)
