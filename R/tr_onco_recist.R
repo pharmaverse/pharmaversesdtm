3529cd3444184be98c11fce4ae3b1a6e4714e3dc
Vladyslav <shuliarv@roche.com>
2024-12-12 10:23:36 +0000

    #111 Fix lintr

--- R/tr_onco_recist.R
+++ R/tr_onco_recist.R
@@ -9,28 +9,26 @@
 #' @title Tumor Results (RECIST 1.1) for Oncology
 #' @description A SDTM TR dataset using RECIST 1.1 intended for examples of ADaM dataset creation
 #' @docType data
-#' @format A data frame with 21 columns:
+#' @format A data frame with 19 columns:
 #'   \describe{
-#'     \item{TRLNKID}{undocumented field}
-#'     \item{TRTESTCD}{undocumented field}
-#'     \item{TRORRES}{undocumented field}
-#'     \item{VISITNUM}{undocumented field}
-#'     \item{TREVALID}{undocumented field}
-#'     \item{TRGRPID}{undocumented field}
-#'     \item{.before}{undocumented field}
-#'     \item{TREVAL}{undocumented field}
-#'     \item{TRACPTFL}{undocumented field}
-#'     \item{.after}{undocumented field}
-#'     \item{USUBJID}{undocumented field}
-#'     \item{TRDTC}{undocumented field}
 #'     \item{DOMAIN}{undocumented field}
 #'     \item{STUDYID}{undocumented field}
+#'     \item{USUBJID}{undocumented field}
+#'     \item{TRGRPID}{undocumented field}
+#'     \item{TRLNKID}{undocumented field}
+#'     \item{TRTESTCD}{undocumented field}
 #'     \item{TRTEST}{undocumented field}
-#'     \item{VISIT}{undocumented field}
+#'     \item{TRORRES}{undocumented field}
 #'     \item{TRORRESU}{undocumented field}
 #'     \item{TRSTRESC}{undocumented field}
 #'     \item{TRSTRESN}{undocumented field}
 #'     \item{TRSTRESU}{undocumented field}
+#'     \item{VISITNUM}{undocumented field}
+#'     \item{VISIT}{undocumented field}
+#'     \item{TREVAL}{undocumented field}
+#'     \item{TREVALID}{undocumented field}
+#'     \item{TRACPTFL}{undocumented field}
+#'     \item{TRDTC}{undocumented field}
 #'     \item{TRSEQ}{undocumented field}
 #'   }
 #'
