b4ce4a8e3a39e53d0b0c5062b21984e8f4f0bd6c
Fanny-Gautier <fanny.gautier@cytel.com>
2025-05-07 12:11:01 +0200

    #157 update EG documentation

--- R/eg.R
+++ R/eg.R
@@ -16,12 +16,12 @@
 #'     \item{DOMAIN}{Domain Abbreviation}
 #'     \item{USUBJID}{Unique Subject Identifier}
 #'     \item{EGSEQ}{Sequence Number}
-#'     \item{EGTESTCD}{Test Code}
-#'     \item{EGTEST}{Test Name}
-#'     \item{EGORRES}{Original Result}
+#'     \item{EGTESTCD}{ECG Test Short Name}
+#'     \item{EGTEST}{ECG Test Name}
+#'     \item{EGORRES}{Result or Finding in Original Units}
 #'     \item{EGORRESU}{Original Units}
-#'     \item{EGSTRESC}{Standard Character Result}
-#'     \item{EGSTRESN}{Standard Numeric Result}
+#'     \item{EGSTRESC}{Character Result/Finding in Std Format}
+#'     \item{EGSTRESN}{Numeric Result/Finding in Standard Units}
 #'     \item{EGSTRESU}{Standard Units}
 #'     \item{EGSTAT}{Completion Status}
 #'     \item{EGLOC}{Location of Vital Signs Measurement}
