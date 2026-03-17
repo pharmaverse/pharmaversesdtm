e520a37c1e24f2d41c5d39e43af32da445815388
Lina2689 <lina.patil@cytel.com>
2026-03-17 11:16:57 +0530

    #214: styler

--- data-raw/dm.R
+++ data-raw/dm.R
@@ -10,8 +10,8 @@ library(lubridate)
 
 # Create dm, suppdm ----
 sdtm_path <- "https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/" # nolint
-#raw_dm <- read_xpt(paste0(sdtm_path, "dm", ".xpt?raw=true"))
-#raw_suppdm <- read_xpt(paste0(sdtm_path, "suppdm", ".xpt?raw=true"))
+# raw_dm <- read_xpt(paste0(sdtm_path, "dm", ".xpt?raw=true"))
+# raw_suppdm <- read_xpt(paste0(sdtm_path, "suppdm", ".xpt?raw=true"))
 
 url <- "https://raw.githubusercontent.com/cdisc-org/sdtm-adam-pilot-project/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt"
 raw_dm <- read_xpt(url)
